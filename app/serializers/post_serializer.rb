44395c12fe334859d140aaf79f9650d86f7f9a02
Adriano Tadao <drianotadao@gmail.com>
2014-06-14 15:26:41 -0300

    modeling its working

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,3 +1,9 @@
 class PostSerializer < ActiveModel::Serializer
-  attributes :title, :body, :author, :categories, :tags
+  attributes :title, :body, :author, :tags
+
+  has_many :comments
+
+  def comments
+    object.comments.published
+  end
 end
