011451fdc0cea0744e2111bcc0d5e2775b5a776c
Adriano Tadao <drianotadao@gmail.com>
2014-06-18 19:31:52 -0300

    code cleaner

--- app/assets/javascripts/routes/post.js
+++ app/assets/javascripts/routes/post.js
@@ -1,13 +1,11 @@
 Blog.PostsRoute = Ember.Route.extend({
     model: function() {
-        window.index = this.store
         return this.store.findAll('post')
     }
 })
 
 Blog.PostsShowRoute = Ember.Route.extend({
     model: function(params) {
-        window.show = this.modelFor('posts')
         return this.modelFor('posts').findBy('slug', params.slug)
     }
 })
