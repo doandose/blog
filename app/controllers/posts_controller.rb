e443623453fe60189cfdc44296e03f3a8cd90905
Adriano Tadao <drianotadao@gmail.com>
2014-06-19 16:33:08 -0300

    improvements comments and tags

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   def index
-    render json: Post.published.recent, status: :ok
+    render json: Post.recent, status: :ok
   end
 
   def show
--- app/models/post.rb
+++ app/models/post.rb
@@ -9,6 +9,6 @@ class Post < ActiveRecord::Base
   belongs_to :category
 
   # scope
+  default_scope -> { where.not(published_at: nil) }
   scope :recent, -> { order(:published_at) }
-  scope :published, -> { where.not(published_at: nil) }
 end
