d4a3de65f33f585fd4c09c24652dbc3ae91a66e7
saulo da silva santiago <saulodasilvasantiago@gmail.com>
2014-06-30 01:08:27 -0300

    configs deploy

--- Gemfile
+++ Gemfile
@@ -2,19 +2,20 @@ source 'https://rubygems.org'
 
 gem 'rails', '4.0.4'
 gem 'mysql2', '0.3.16'
-gem 'active_model_serializers', '~> 0.8.0'
+gem 'active_model_serializers', '0.8.0'
 
 gem 'handlebars_assets', '0.16'
 gem 'haml', '4.0.5'
-gem 'hamlbars', '~> 2.1'
+gem 'hamlbars', '2.1'
 
-gem 'friendly_id', '~> 5.0.0'
-gem 'stylus', '~> 1.0.1'
+gem 'friendly_id', '5.0.0'
+gem 'stylus', '1.0.1'
 
 group :assets do
-  gem 'uglifier', '>= 1.3.0'
+  gem 'uglifier', '1.3.0'
 end
 
 group :development do
   gem 'faker', '1.3.0'
+  gem 'capistrano', '3.2.0'
 end
---
+++ config/deploy.rb
@@ -0,0 +1,40 @@
+# config valid only for Capistrano 3.1
+lock '3.2.0'
+
+set :application, 'blog'
+set :repo_url, 'git@github.com:doandose/blog.git'
+set :branch, 'master'
+
+set :deploy_to, '/home/blog/www/'
+set :scm, :git
+
+set :format, :pretty
+set :log_level, :debug
+set :pty, true
+set :keep_releases, 3
+
+set :nvm_path, '/home/blog/.nvm'
+set :nvm_node, 'v0.10.10'
+set :nvm_map_bins, %w{node npm bower}
+
+
+namespace :deploy do
+
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      execute :touch, release_path.join('tmp/restart.txt')
+    end
+  end
+
+  after 'npm:install', :copy_env do
+    on roles(:app) do
+      execute <<-CMD
+      CMD
+    end
+  end
+
+  after 'bower:install', 'grunt'
+
+  after :publishing, :restart
+end
