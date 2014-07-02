ad3b147f87355fbd3437c935aa9567903c5c9ee3
Adriano Tadao <drianotadao@gmail.com>
2014-07-02 20:46:21 -0300

    added description on post, and making the layout"

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,7 +1,7 @@
 class PostSerializer < ActiveModel::Serializer
   embed :ids, include: true
 
-  attributes :id, :title, :body, :published_at, :slug, :category_id, :author
+  attributes :id, :title, :description, :body, :published_at, :slug, :category_id, :author
 
   has_many :tags
   has_many :comments
