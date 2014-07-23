f043cd4ea7b47bb76c1abba19d9b847e8716d4f2
Adriano Tadao <drianotadao@gmail.com>
2014-07-22 21:57:45 -0300

    [pagination] testing the css in page number

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,17 +4,18 @@
 end
 
 # categories
-10.times do
-  Category.create(title: Faker::Lorem.sentence(2, true, 1))
+colors = %w(pomegranate alizarin nephritis silver wisteria belize_hole orange)
+6.times do
+  Category.create(title: Faker::Lorem.word, dominant_color: colors.sample)
 end
 
 # tags
 10.times do
-  Tag.create(name: Faker::Lorem.sentence(6))
+  Tag.create(name: Faker::Lorem.word)
 end
 
 # posts
-50.times do
+100.times do
   Post.create(
     published_at: [nil, DateTime.now + rand(99).minutes].sample,
     title:        Faker::Lorem.sentence(6),
